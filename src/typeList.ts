1211710a7d5a28112b0b1095f0a4a27276c2c8c5
Sena <73450328+10Ne2Cx1To426@users.noreply.github.com>
2021-03-21 23:17:15 +0900

    commit

---
+++ src/typeList.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/** 基本型 */
+
+// boolean
+let bool: boolean = true;
+
+// number
+let num: number = 0;
+
+// string
+let str: string = "A";
+
+// Array
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+// tuple
+let tuple: [number, string] = [0, "A"];
+
+// any
+let any1: any = false;
+
+// void
+const funcA = (): void => {
+  const test = "TEST";
+};
+
+// null
+let nul1: null = null;
+
+// undifined
+let undifinded1: undefined = undefined;
+
+// object
+let obj1: object = {};
+let obj: { id: number; name: string } = { id: 0, name: "tauyong" };
